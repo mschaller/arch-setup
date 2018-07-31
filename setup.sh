3375a65b16a920bfeb2061f6a51db3a5024219b4
Michael Schaller <michael.schaller@swednet.de>
2018-07-31 11:05:35 +0200

    Add ENV-VARS and stage2-script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,59 @@
 #!/bin/bash
 
-loadkeys de
+if [ ! -n "${NAMEUSER+1}" ]; then
+    NAMEUSER=kaputtnik
+fi
+
+if [ ! -n "${NAMEHOST+1}" ]; then
+    NAMEHOST=arch-vbox
+fi
+
+if [ ! -n "${NAMEDOMAIN+1}" ]; then
+    NAMEDOMAIN=0 # 0 is special, meaning none.
+fi
+
+if [ ! -n "${NAMEDEVICE+1}" ]; then
+    NAMEDEVICE=/dev/sda
+fi
+
+if [ ! -n "${NAMETIMEZONE+1}" ]; then
+    NAMETIMEZONE=Europe/Berlin
+else
+    if [ ! -f /usr/share/zoneinfo/$NAMETIMEZONE ]; then
+        echo $NAMETIMEZONE is not a legal TZ value. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+        exit 2
+    fi
+fi
+
+if [ ! -n "${NAMELOCALE+1}" ]; then
+    NAMELOCALE=en_US.UTF-8
+else
+    if [[ $(grep "$NAMELOCALE" /etc/locale.gen | wc -l) == 0 ]]; then
+        echo $NAMELOCALE is not legal. See /etc/locale.gen
+        exit 3
+    fi
+fi
+
+if [ ! -n "${NAMEKEYMAP+1}" ]; then
+    NAMEKEYMAP=de-latin1
+fi
+
 timedatectl set-ntp true
-parted /dev/sda mklabel msdos
-parted /dev/sda mkpart primary ext4 0% 100%
-parted /dev/sda set 1 boot on
-mkfs.ext4 /dev/sda1
-mount /dev/sda1 /mnt
+parted $NAMEDEVICE mklabel msdos
+parted $NAMEDEVICE mkpart primary ext4 0% 100%
+parted $NAMEDEVICE set 1 boot on
+mkfs.ext4 ${NAMEDEVICE}1
+mount ${NAMEDEVICE}1 /mnt
 cat /etc/pacman.d/mirrorlist | grep .de/ > /etc/pacman.d/mirrorlist
 pacstrap /mnt base base-devel wget
 genfstab -U /mnt > /mnt/etc/fstab
 
-arch-chroot /mnt /bin/bash
+echo "NAMEUSER=$NAMEUSER\nNAMELOCALE=$NAMELOCALE\nNAMEDOMAIN=$NAMEDOMAIN\nNAMETIMEZONE=$NAMETIMEZONE\nNAMELOCALE=$NAMELOCALE\nNAMEKEYMAP=$NAMEKEYMAP\nNAMEDEVICE=$NAMEDEVICE" > /mnt/root/stage2.env
+
+wget -nv -O /mnt/root/stage2.sh https://raw.githubusercontent.com/mschaller/arch-setup/master/stage2.sh \
+    && chmod u+x /mnt/root/stage2.sh
+
+arch-chroot /mnt /root/stage2.sh
+
+rm /mnt/root/stage2.env
+rm /mnt/root/stage2.sh
---
+++ stage2.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+source /root/stage2.env
+
+ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+hwclock --systohc -utc
+
+sed -i "s/^#$NAMELOCALE/$NAMELOCALE/" /etc/locale.gen
+if [[ $? != 0 ]]; then
+    echo Failed to modify /etc/locale.gen
+    exit 
+fi
+if [[ $(grep "^$NAMELOCALE" /etc/locale.gen | wc -l) == 0 ]]; then
+    echo Failed to modify /etc/locale.gen
+    exit 
+fi
+locale-gen
+
+echo "LANG=$NAMELOCALE" > /etc/locale.conf
+echo "KEYMAP=$NAMEKEYMAP" > /etc/vconsole.conf
+
+echo SUBSYSTEM==\"net\", ACTION==\"add\", ATTR{address}==\"`ip addr show \`ls /sys/class/net --color=never | egrep "^wl|^en"\` | grep link/ | awk '{print $2}'`\", NAME=\"net1\" > /etc/udev/rules.d/10-network.rules && cat /etc/udev/rules.d/10-network.rules
+if [[ $? != 0 ]]; then
+    echo Failed to rename network device to net1
+    exit
+fi
+
+systemctl enable dhcpcd@net1.service
+if [[ $? != 0 ]]; then
+    echo Failed to enable net1
+    exit
+fi
+
+echo $NAMEHOST > /etc/hostname
+
+pacman -S --noconfirm grub os-prober
+
+mkinitcpio -p linux
+if [[ $? != 0 ]]; then
+    echo Failed to create initcpio
+    exit
+fi
+
+grub-install --recheck $NAMEDEVICE
+if [[ $? != 0 ]]; then
+    echo Failed to install GRUB
+    exit
+fi
+
+grub-mkconfig -o /boot/grub/grub.cfg
+if [[ $? != 0 ]]; then
+    echo Failed to configure GRUB
+    exit
+fi
+
+
